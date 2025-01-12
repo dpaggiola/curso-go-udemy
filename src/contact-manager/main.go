58dc951642746439849396329802a5adbcaddfdb
Daniel Paggiola <dpaggiola@gmail.com>
2025-01-12 16:08:42 -0300

    Class 44

---
+++ src/contact-manager/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	// "errors"
+	"fmt"
+	// "strconv"
+)
+
+func divide(dividendo, divisor int) (int, error){
+	if divisor == 0 {
+		// return 0, errors.New("No se puede dividir por cero")
+		return 0, fmt.Errorf("No se puede dividir por cero")
+	}
+	return dividendo / divisor, nil
+}
+
+func main() {
+	// str := "123"
+	// num, err := strconv.Atoi(str)
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// 	return
+	// }
+
+	// fmt.Println("Número: ", num)
+	result, err := divide(10,0)
+	if err != nil{
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("Resultado: ", result)
+}
