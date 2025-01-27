c5253555b9c3ca48ce190b2abe91ddf0c4fb59cd
Daniel Paggiola <dpaggiola@gmail.com>
2025-01-27 15:39:51 -0300

    Class 128

--- src/rpsweb/handlers/handlers.go
+++ src/rpsweb/handlers/handlers.go
@@ -2,11 +2,22 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Página de inicio")
+	tpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
+	if err != nil {
+		http.Error(w, "Error al analizar plantillas", http.StatusInternalServerError)
+		return
+	}
+
+	err = tpl.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		return
+	}
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
