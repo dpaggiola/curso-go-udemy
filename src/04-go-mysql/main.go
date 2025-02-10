b34b1bacbfd11377545b3a747c7ce191f661b640
Daniel Paggiola <dpaggiola@gmail.com>
2025-02-10 15:35:32 -0300

    Class 170

--- src/04-go-mysql/main.go
+++ src/04-go-mysql/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gomysql/db"
 	"gomysql/models"
 )
@@ -9,8 +10,19 @@ func main() {
 	db.Connect()
 	// db.Ping()
 
-	db.ExistsTable("users")
-	db.CreateTable(models.UserSchema, "users")
+	// db.ExistsTable("users")
+	// db.CreateTable(models.UserSchema, "users")
+	// db.TruncateTable("users")
+	// user := models.CreateUser("otro", "1234", "otro@gmail.com")
+	// fmt.Println(user)
+	user := models.GetUser(2)
+	fmt.Println(user)
+	// user.Username = "juan"
+	// user.Password = "juan2451"
+	// user.Email = "juan@gmail.com"
+	// user.Save()
+	// fmt.Println(models.ListUsers())
+	user.Delete()
 
 	db.Close()
 }
