74f8b2c47fbe8b02f75c29acf700b5f9269ef28a
Daniel Paggiola <dpaggiola@gmail.com>
2025-02-11 16:30:56 -0300

    Class 178

--- src/05-api-rest/handlers/handlers.go
+++ src/05-api-rest/handlers/handlers.go
@@ -1,20 +1,61 @@
 package handlers
 
 import (
+	"apirest/db"
+	"apirest/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Lista todos los usuarios")
+	rw.Header().Set("Content-Type", "application/json")
+
+	db.Connect()
+	users := models.ListUsers()
+	db.Close()
+
+	output, _ := json.Marshal(users)
+	fmt.Fprintln(rw, string(output))
 }
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Obtiene un usuario")
+	rw.Header().Set("Content-Type", "application/json")
+
+	// Obtener ID
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+
+	
+	db.Connect()
+	user := models.GetUser(userId)
+	db.Close()
+
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Crea un usuario")
+	rw.Header().Set("Content-Type", "application/json")
+
+	// Obtener registro
+	user := models.User{}
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&user); err != nil {
+		fmt.Fprintln(rw, http.StatusUnprocessableEntity)
+	} else {
+		db.Connect()
+		user.Save()
+		db.Close()
+	} 
+
+	output, _ := json.Marshal(user)
+	fmt.Fprintln(rw, string(output))
 }
 
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
