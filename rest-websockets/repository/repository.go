2b42f16d2db2880f727992dc9c8fde6f2f3e973c
Daniel Paggiola <dpaggiola@gmail.com>
2025-06-11 11:01:13 -0300

    Clase 17

--- rest-websockets/repository/repository.go
+++ rest-websockets/repository/repository.go
@@ -6,16 +6,17 @@ import (
 	"platzi.com/go/rest-ws/models"
 )
 
-type UserRepository interface {
+type Repository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id string) (*models.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*models.User, error)
+	InsertPost(ctx context.Context, post *models.Post) error
 	Close() error
 }
 
-var implementation UserRepository
+var implementation Repository
 
-func SetRepository(repository UserRepository) {
+func SetRepository(repository Repository) {
 	implementation = repository
 }
 
@@ -35,3 +36,6 @@ func Close() error {
 	return implementation.Close()
 }
 
+func InsertPost(ctx context.Context, post *models.Post) error {
+	return implementation.InsertPost(ctx, post)
+}
